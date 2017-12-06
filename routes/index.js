5b3f5e65a85e94250aeb7026e964e76a187dac8e
pawan <pawanyadavsultania@gmail.com>
2017-12-06 22:29:18 +0530

    uploading videos and images

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.post('/edit',function (req,res,) {
+    
+})
 module.exports = router;
