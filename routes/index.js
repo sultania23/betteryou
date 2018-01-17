b5fb056d04e23035ca88ddae6a15b29704c611b9
pawan <pawanyadavsultania@gmail.com>
2018-01-17 20:39:35 +0530

    more fixes

--- routes/index.js
+++ routes/index.js
@@ -3,33 +3,38 @@ var router = express.Router();
 var multer = require('multer');
 var upload = multer({ dest: 'uploads/' });
 var firebase = require('firebase');
+var fs = require('fs');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/upload',upload.any(),function (req,res) {
+router.post('/upload',upload.single('image'),function (req,res) {
+    var data ={
+        title: req.body.title,
+        tags: req.body.tags,
+        cat: req.body.cat,
+        subcat: req.body.subcat,
+        time: req.body.time,
+    };
 
-    if (!req.files) {
-        console.log("no files found here");
+
+    if (!req.file) {
         res.send("not files");
     }
-    else
-    {
-        //res.writeHead(200, {"Content-Type": "application/json"});
-        //res.send(req.body.cat);
-        var url = firebase.database().ref('images').push(req.files);
-        var data = {
-            cat: req.body.cat,
-            subcat: req.body.subcat,
-            url: url,
-            title:req.body.title,
-            tags:req.body.tags,
-            time:req.body.time
-        };
-        //
-        res.render('editor-content.ejs',data);
+    else {
+        var file = req.file;
+        var image_ref = firebase.database().ref('/images').push(req.file);
+
+        if(image_ref)
+        {
+            data.url =  image_ref;
+        }
+        res.render('editor-content',data);
     }
 });
+
+
 module.exports = router;
